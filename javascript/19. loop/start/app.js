4d9fdfbf27762a7d8b86d93e1e9cc68748d6bd0f
zkzkdlqm <zkzkdlqm@gmail.com>
2022-12-16 21:21:31 +0900

    22.12.15 까먹은 커밋

---
+++ javascript/19. loop/start/app.js
@@ -0,0 +1,37 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+// console.log(11);
+
+// for (let index = 0; index < 100; index += 2) {
+//   console.log(index);
+// }
+
+// for (let index = 0; index < 100; index--) {
+//   console.log("error");
+// }
+
+// let input = parseInt(prompt("enter number(1~20)"));
+// let answer = Math.floor(Math.random() * 20);
+// console.log(answer);
+// while (input !== answer) {
+//   console.log(answer);
+//   input = parseInt(prompt("try again! enter number(1~200)"));
+// }
+
+function randArr(){
+    const arr= [];
+    let arrLength = Math.floor(Math.random()*100)
+    let wordLength = Math.floor(Math.random()*100)
+    for(let i=0;i<arrLength;i++){
+        arr.
+    }
+    return arr;
+}
