00cc54f1d6f38e6e52aece4049ebfd72b2880f97
zkzkdlqm <zkzkdlqm@gmail.com>
2022-12-22 12:31:28 +0900

    2022.12.22 save

---
+++ javascript/24. Dom/start/app.js
@@ -0,0 +1,25 @@
+// const allImage = document.getElementsByTagName("img");
+
+// const squareImage = document.getElementsByClassName("square");
+
+// for (let img of squareImage) {
+//   img.src =
+//     "https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80";
+// }
+
+// const link = document.querySelectorAll("p a");
+
+// for (let i of link) {
+//   console.log(i.href);
+// }
+// const allLinks = document.querySelectorAll("a");
+
+// for (let link of allLinks) {
+//   link.innerText = "I AM LINK!!!";
+// }
+// const allLinks = document.querySelectorAll("a");
+// for (let link of allLinks) {
+//   link.style.color = "rgb(0,108.134)";
+//   link.style.textDecorationColor = "magenta";
+//   link.style.textDecorationStyle = "wavy";
+// }
